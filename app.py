d3be564bad39f826b78cc87a676c3d1f764798f9
zexpher <fahriangga30@gmail.com>
2022-11-03 21:49:08 +0700

    Memperbarui main file python

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # ====== FILE INI UNTUK MENJALANKAN FUNCTION ====== #
 
-# import function main dari file function
+# import function main dari file function.py
 from function import main
 
-# Me-looping terus sampe ketemu nomer 0
+# Me-looping terus sampe ketemu angka 0
 while True:
-# Memanggil function main dari file function
+# Memanggil function main dari file function.py
     main()
