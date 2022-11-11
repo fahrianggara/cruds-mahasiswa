9e96e6406b07c6653d9e173ecae099f815c109ba
zexpher <fahriangga30@gmail.com>
2022-11-12 03:50:11 +0700

    Ok...

--- function/with_comments/app.py
+++ function/with_comments/app.py
@@ -6,7 +6,7 @@ from function.main import main
 # __name__ adalah variabel khusus yang akan bernilai __main__ jika file ini dijalankan
 # fungsinya adalah untuk memastikan bahwa file ini dijalankan sebagai file utama dan bukan sebagai file yang diimport
 if __name__ == '__main__':
-    # Me-looping terus sampe ketemu angka 0
+    # Me-looping terus menerus sampai user memilih keluar dari menu
     while True:
     # Memanggil function main dari file function.py
         main()
